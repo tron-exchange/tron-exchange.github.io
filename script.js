22688f67b0fccf8c1e1aa0853b62bdb7b5f58848
Tron <mmoney.work.01@gmail.com>
2024-10-14 11:18:13 +0300

    .

--- script.js
+++ script.js
@@ -59,3 +59,12 @@ function startExchange() {
         document.getElementById("error-message").style.display = "block";
     }
 }
+
+function toggleAnswer(id) {
+    var answer = document.getElementById("answer-" + id);
+    if (answer.style.display === "none") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
